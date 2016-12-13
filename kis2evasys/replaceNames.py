632ef024a96607584e205feabd679f7fb4df9f7b
Christian De Schryver <schryver@eit.uni-kl.de>
2016-12-13 14:32:33 +0100

    README updated, moved to subfolder for merge with VUKL

--- kis2evasys/replaceNames.py
+++ kis2evasys/replaceNames.py
@@ -3,6 +3,21 @@
 	sets correct values for full names and titles in EvaSys import file
 '''
 
+# This file is part of VUKL.
+#
+# VUKL is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# VUKL is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with VUKL.  If not, see <http://www.gnu.org/licenses/>.
+
 __author__ = "Christian De Schryver"
 
 import csv
