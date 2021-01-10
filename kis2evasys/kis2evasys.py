3f0ba2da6f07dfdb6ff8ac16ad8fdc5c245b7856
Christian De Schryver <schryver@eit.uni-kl.de>
2021-01-10 11:41:42 +0100

    kis2evasys Update 2019 SS

--- kis2evasys/kis2evasys.py
+++ kis2evasys/kis2evasys.py
@@ -22,11 +22,11 @@ KIS2EvaSys - Umfragen aus KIS-Veranstaltungen anlegen
 __author__ = "Clemens Reibetanz, Christian De Schryver"
 
 # Bitte die 3 Werte eintragen
-SEMESTER = "[WS16/17]"
-# 27=WS15/16, 28=SS16, 29=WS16/17, 30=SS17
-PERIOD = 29
+SEMESTER = "[SS19]"
+# 27=WS15/16, 28=SS16, 29=WS16/17, 30=SS17, 31=WS17/18, 32=SS18, 33=WS18/19, 34=SS19
+PERIOD = 34
 # URL von KIS -> Studiengänge und Veranstaltungen -> Elektrotechnik und Informationstechnik -> Elektrotechnik und Informationstechnik -> Dozent
-URL = 'http://www.kis.uni-kl.de/campus/all/eventlist.asp?mode=field&gguid=0xFA55C0FA8922324FBE140B703F1FBE45&tguid=0x5CD863108B761F4CB159DA9ABDCAAAE8'
+URL = 'https://www.kis.uni-kl.de/campus/all/eventlist.asp?mode=field&gguid=0x090A152C497B43FE801EDB13F2F5CA60&tguid=0x20A24741AF3347FEB6943B4BA195534F'
 # KIS Zeichensatz wie im HTML-Meta-Tag festgelegt
 URLENCODING = "iso-8859-1"
 
